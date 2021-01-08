28bb1786e7eaaa0d3050d2c5cb2464cb9a98f95f
Mary Jo Gray <mj81000@gmail.com>
2021-01-08 21:54:18 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,7 +12,7 @@ def input_to_index(input)
   input.to_i-1
 end
 
-def move(index, board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
-  board[index] = 'X'
+def move(index, board = [" ", " ", " ", " ", " ", " ", " ", " ", " "], fill="X")
+  board[index] = fill
   return board
 end
